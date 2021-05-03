965a65acb0e4132920e5c5411701c856282a9a58
Michał Święciło <michal_00-00@o2.pl>
2021-05-03 11:50:47 +0200

    Funkcje wykrywania zer niezależnych i wykreślania ich minimalną liczbą lini

--- Algorytm węgierski/main.py
+++ Algorytm węgierski/main.py
@@ -1,16 +1,57 @@
-# This is a sample Python script.
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from typing import List
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def independent_zeros(matrix: List[List[float]]):
+    size = len(matrix)
+    visited_rows = []
+    visited_cols = []
+    zeros_position = []
+    for row in range(size):
+        for col in range(size):
+            if (matrix[row][col] == 0) and (row not in visited_rows) and (col not in visited_cols):
+                zeros_position.append((row, col))
+                visited_rows.append(row)
+                visited_cols.append(col)
+    return zeros_position
+
+
+def deleting_independent_zeros(A):
+    wiersze = []
+    kolumny = []
+    f = True
+    A_ = A[:]
+    while f:
+        f = False
+        for i in range(len(A)):
+            if 'o*' not in A[i] and i not in wiersze:
+                wiersze.append(i)
+                f = True
+        for j in range(len(A[0])):
+            for k in wiersze:
+                if A[k][j] is None and j not in kolumny:
+                    kolumny.append(j)
+                    f = True
+        for i in range(len(A)):
+            for k in kolumny:
+                if A[i][k] == 'o*' and i not in wiersze:
+                    wiersze.append(i)
+                    f = True
+    wiersze2 = []
+    for i in range(len(A)):
+        if i not in wiersze:
+            wiersze2.append(i)
+    return wiersze2, kolumny
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    test_matrix = [[5, 4, 8, 6],
+             [0, 6, 0, 6],
+             [4, 3, 2, 7],
+             [3, 6, 0, 2]]
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print(independent_zeros(test_matrix))
+
--- Algorytm węgierski/project_venv/Scripts/activate.bat
+++ Algorytm węgierski/project_venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\micha\OneDrive\Pulpit\E-learning\Semestr 4\Badania operacyjne\Ćwiczenie 8\Algorytm węgierski\project_venv
+set VIRTUAL_ENV=C:\Users\micha\OneDrive\Pulpit\E-learning\Semestr 4\Badania operacyjne\Ćwiczenie 8\Projekt-BO\Algorytm węgierski\project_venv
 
 if not defined PROMPT set PROMPT=$P$G
 
